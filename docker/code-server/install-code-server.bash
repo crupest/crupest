9ad128293f286db5df42658ace85772b635b17f8
crupest <crupest@outlook.com>
2022-11-29 14:31:09 +0800

    Try to fix code-server. v8.0

--- docker/code-server/install-code-server.bash
+++ docker/code-server/install-code-server.bash
@@ -2,6 +2,11 @@
 
 set -e
 
+apt-get update
+apt-get install -y locales curl
+rm -rf /var/lib/apt/lists/* \
+localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8
+
 VERSION=$(curl -s https://api.github.com/repos/coder/code-server/releases/latest | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
 
 curl -fOL "https://github.com/coder/code-server/releases/download/v${VERSION}/code-server_${VERSION}_amd64.deb"
