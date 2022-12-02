e2bd8d8048243ca8a5c3e07db2fe14b093c41870
crupest <crupest@outlook.com>
2022-12-02 13:38:28 +0800

    Fix #11.

---
+++ docker/crupest-blog/install-hugo.bash
@@ -0,0 +1,4 @@
+VERSION=$(curl -s https://api.github.com/repos/gohugoio/hugo/releases/latest | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
+url="https://github.com/gohugoio/hugo/releases/download/v${VERSION}/hugo_extended_${VERSION}_linux-amd64.tar.gz"
+curl -fL -o /root/hugo.tar.gz "${url}"
+tar -xzf /root/hugo.tar.gz -C /usr/bin hugo
