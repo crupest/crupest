e48986d206bef7a5177645ac19e1a996f9874a3c
crupest <crupest@outlook.com>
2022-12-02 13:54:46 +0800

    Try to fix hugo.

--- docker/crupest-blog/install-hugo.bash
+++ docker/crupest-blog/install-hugo.bash
@@ -6,3 +6,5 @@ VERSION=$(curl -s https://api.github.com/repos/gohugoio/hugo/releases/latest | g
 url="https://github.com/gohugoio/hugo/releases/download/v${VERSION}/hugo_extended_${VERSION}_linux-amd64.tar.gz"
 curl -fL -o /root/hugo.tar.gz "${url}"
 tar -xzf /root/hugo.tar.gz -C /usr/bin hugo
+
+hugo version
