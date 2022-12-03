d56c005e473d1015efc1809842c4f805d7b11b7e
crupest <crupest@outlook.com>
2022-12-03 17:39:38 +0800

    Add git to blog container.

--- docker/crupest-blog/install-hugo.bash
+++ docker/crupest-blog/install-hugo.bash
@@ -3,7 +3,7 @@
 set -e
 
 apt-get update
-apt-get install -y tini locales curl
+apt-get install -y tini locales curl git
 rm -rf /var/lib/apt/lists/*
 localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8
 
