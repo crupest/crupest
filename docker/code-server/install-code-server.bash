90d15476fbac47b7d37221cb8b35d747fb87fbc2
crupest <crupest@outlook.com>
2022-12-03 10:51:41 +0800

    Fix hugo image too big issue.

--- docker/code-server/install-code-server.bash
+++ docker/code-server/install-code-server.bash
@@ -9,6 +9,14 @@ localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8
 
 VERSION=$(curl -s https://api.github.com/repos/coder/code-server/releases/latest | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
 
-curl -fOL "https://github.com/coder/code-server/releases/download/v${VERSION}/code-server_${VERSION}_amd64.deb"
+echo "The latest version of code-server is ${VERSION}."
+
+url="https://github.com/coder/code-server/releases/download/v${VERSION}/code-server_${VERSION}_amd64.deb"
+
+echo "Download code-server from $url."
+
+curl -sSfOL "$url"
 dpkg -i "code-server_${VERSION}_amd64.deb"
 rm "code-server_${VERSION}_amd64.deb"
+
+echo "Code-server version: $(code-server --version)."
