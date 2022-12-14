94d39ea715ae81ef93e1bc289762b7500db197f9
crupest <crupest@outlook.com>
2022-12-14 15:58:41 +0800

    Develop secret api. v38

--- docker/crupest-api/CrupestApi/CrupestApi.Commons/Crud/CrudWebApplicationExtensions.cs
+++ docker/crupest-api/CrupestApi/CrupestApi.Commons/Crud/CrudWebApplicationExtensions.cs
@@ -2,4 +2,16 @@ namespace CrupestApi.Commons.Crud;
 
 public static class CrudWebApplicationExtensions
 {
+    public static WebApplication UseCrud<TEntity>(this WebApplication app, string path) where TEntity : class
+    {
+        app.MapGet(path, async (context) =>
+        {
+            var crudService = context.RequestServices.GetRequiredService<CrudService<TEntity>>();
+            var entityJsonHelper = context.RequestServices.GetRequiredService<EntityJsonHelper<TEntity>>();
+            var allEntities = crudService.GetAll();
+            await context.ResponseJsonAsync(allEntities.Select(e => entityJsonHelper.ConvertEntityToDictionary(e)));
+        });
+
+        return app;
+    }
 }
