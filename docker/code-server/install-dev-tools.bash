9edb088c2e2a8912f5b836215ae31ff0dc11f526
crupest <crupest@outlook.com>
2022-12-20 20:04:56 +0800

    Fix code-server. v3

--- docker/code-server/install-dev-tools.bash
+++ docker/code-server/install-dev-tools.bash
@@ -12,6 +12,7 @@ wget https://packages.microsoft.com/config/debian/11/packages-microsoft-prod.deb
 dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
 
+apt-get update
 apt-get install -y dotnet-sdk-7.0
 
 rm -rf /var/lib/apt/lists/*
