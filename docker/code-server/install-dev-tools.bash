c7d01d5ce091f0c2ff5f35bf59496b29cbab517e
crupest <crupest@outlook.com>
2022-12-20 11:54:42 +0000

    Update code-server image.

---
+++ docker/code-server/install-dev-tools.bash
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+set -e
+
+apt-get install vim wget git
+
+git config --global user.email "$GIT_EMAIL"
+git config --global user.name "$GIT_NAME"
+
+wget https://packages.microsoft.com/config/debian/11/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+
+apt-get install dotnet-sdk-7.0
