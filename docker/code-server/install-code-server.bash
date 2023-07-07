d61b3476962fd1e27911fc73550e6ec52debd316
crupest <crupest@outlook.com>
2023-07-07 21:12:03 +0800

    Code server no longer install softwares.
    Install them yourself!

--- docker/code-server/install-code-server.bash
+++ docker/code-server/install-code-server.bash
@@ -4,7 +4,7 @@ set -e
 
 apt-get update
 apt-get install -y locales curl
-rm -rf /var/lib/apt/lists/*
+
 localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8
 
 VERSION=$(curl -s https://api.github.com/repos/coder/code-server/releases/latest | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
@@ -20,3 +20,5 @@ dpkg -i "code-server_${VERSION}_amd64.deb"
 rm "code-server_${VERSION}_amd64.deb"
 
 echo "Code-server version: $(code-server --version)."
+
+rm -rf /var/lib/apt/lists/*
