3a3ba2996b2041ac4e40dd6082b70afebd306e07
crupest <crupest@outlook.com>
2023-07-07 21:12:03 +0800

    Code server no longer install softwares.
    Install them yourself!

--- docker/code-server/install-dev-tools.bash
+++ docker/code-server/install-dev-tools.bash
@@ -6,11 +6,9 @@ apt-get update
 apt-get install -y vim lsb-release wget git software-properties-common gnupg
 apt-get install -y gcc g++ make gdb
 
-git config --global user.email "$GIT_EMAIL"
-git config --global user.name "$GIT_NAME"
+# git config --global user.email "$GIT_EMAIL"
+# git config --global user.name "$GIT_NAME"
 
 source ./install-llvm.bash
 source ./install-cmake.bash
 source ./install-dotnet.bash
-
-rm -rf /var/lib/apt/lists/*
