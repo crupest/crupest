af9b310db9df801c6def861966be9a265d1f5aed
crupest <crupest@outlook.com>
2023-09-07 00:28:36 +0800

    Update nvim config.

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -1,2 +1,13 @@
 vim.cmd.cd("~");
 vim.opt.shell = "pwsh";
+
+vim.opt.softtabstop = 4;
+vim.opt.shiftwidth = 4;
+vim.opt.expandtab = true;
+
+if vim.g.neovide then
+    vim.g.neovide_transparency = 0.95;
+    vim.g.neovide_input_ime = false;
+    vim.g.neovide_cursor_vfx_mode = "railgun";
+end
+
