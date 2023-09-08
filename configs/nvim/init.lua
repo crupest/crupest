e2c8efa08826eb694e8e293a14e0f29fc48f6c31
crupest <crupest@outlook.com>
2023-09-08 20:18:59 +0800

    Update neovim config.

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -285,3 +285,12 @@ require("catppuccin").setup{
 
 vim.cmd.colorscheme "catppuccin"
 
+vim.keymap.set("n", "<esc>", function ()
+    local wins = vim.api.nvim_list_wins()
+    for _, v in ipairs(wins) do
+        if vim.api.nvim_win_get_config(v).relative ~= '' then
+            vim.api.nvim_win_close(v, false)
+        end
+    end
+end)
+
