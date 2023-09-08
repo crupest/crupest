e1a0328670862a8a0408599d8b0808606fd63381
crupest <crupest@outlook.com>
2023-09-08 17:57:37 +0800

    Update nvim config.

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -167,11 +167,14 @@ cmp.setup({
     window = {
     },
     mapping = cmp.mapping.preset.insert({
+        ['<C-j>'] = cmp.mapping.select_next_item(),
+        ['<C-k>'] = cmp.mapping.select_prev_item(),
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
         ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ['<tab>'] = cmp.mapping.confirm({ select = true })
     }),
     sources = cmp.config.sources({
         { name = 'nvim_lsp' },
