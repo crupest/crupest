5eb8bc5d179a9ec10260f0aeb62c51e36ab96b28
crupest <crupest@outlook.com>
2023-09-08 17:57:37 +0800

    Update nvim config. Capabilities bug.

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -219,9 +219,16 @@ lspconfig.lua_ls.setup {
 }
 
 -- setup lsp frontend
-lspconfig.cssls.setup {}
-lspconfig.html.setup {}
+lspconfig.cssls.setup {
+    capabilities = capabilities
+}
+
+lspconfig.html.setup {
+    capabilities = capabilities
+}
+
 lspconfig.tsserver.setup{
+    capabilities = capabilities,
     on_new_config = function (new_config, new_root_dir)
         local local_tsserver = require("crupest-util").find_npm_exe(new_root_dir, "typescript-language-server");
         if local_tsserver then
