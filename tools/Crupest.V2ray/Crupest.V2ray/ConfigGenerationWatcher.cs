f1dc756c2b741ab2fcbaa9a3c41dfab4a93ee61b
crupest <crupest@outlook.com>
2023-09-24 17:22:18 +0800

    Upgrade v2ray.

--- tools/Crupest.V2ray/Crupest.V2ray/ConfigGenerationWatcher.cs
+++ tools/Crupest.V2ray/Crupest.V2ray/ConfigGenerationWatcher.cs
@@ -2,17 +2,18 @@ namespace Crupest.V2ray;
 
 public class ConfigGenerationWatcher
 {
-    public ConfigGenerationWatcher() : this(Program.ExeDir, Program.ConfigTemplateFileName, Program.VmessConfigFileName, Program.ProxyConfigFileName, Path.Combine(Program.ExeDir, Program.ConfigOutputFileName), new List<string>())
+    public ConfigGenerationWatcher() : this(Program.ExeDir, Program.ConfigTemplateFileName, Program.VmessConfigFileName, Program.ProxyConfigFileName, Program.HostsConfigFileName, Path.Combine(Program.ExeDir, Program.ConfigOutputFileName), new List<string>())
     {
 
     }
 
-    public ConfigGenerationWatcher(string directory, string configTemplateFileName, string vmessConfigFileName, string proxyConfigFileName, string configOutputPath, List<string> otherWatchFiles)
+    public ConfigGenerationWatcher(string directory, string configTemplateFileName, string vmessConfigFileName, string proxyConfigFileName, string hostsConfigFileName, string configOutputPath, List<string> otherWatchFiles)
     {
         Directory = directory;
         ConfigTemplateFileName = configTemplateFileName;
         VmessConfigFileName = vmessConfigFileName;
         ProxyConfigFileName = proxyConfigFileName;
+        HostsConfigFileName = hostsConfigFileName;
         ConfigOutputPath = configOutputPath;
         OtherWatchFiles = otherWatchFiles;
     }
@@ -21,18 +22,20 @@ public class ConfigGenerationWatcher
     public string ConfigTemplateFileName { get; set; }
     public string VmessConfigFileName { get; set; }
     public string ProxyConfigFileName { get; set; }
+    public string HostsConfigFileName { get; set; }
     public List<string> OtherWatchFiles { get; set; }
     public string ConfigOutputPath { get; set; }
 
     public string ConfigTemplateFilePath => Path.Combine(Directory, ConfigTemplateFileName);
     public string VmessConfigFilePath => Path.Combine(Directory, VmessConfigFileName);
     public string ProxyConfigFilePath => Path.Combine(Directory, ProxyConfigFileName);
+    public string HostsConfigFilePath => Path.Combine(Directory, HostsConfigFileName);
 
     public delegate void OnConfigChangedHandler();
 
     public void Generate()
     {
-        var config = V2rayConfig.FromFiles(ConfigTemplateFilePath, VmessConfigFilePath, ProxyConfigFilePath);
+        var config = V2rayConfig.FromFiles(ConfigTemplateFilePath, VmessConfigFilePath, ProxyConfigFilePath, HostsConfigFilePath);
 
         File.WriteAllText(ConfigOutputPath, config.ToJson());
     }
