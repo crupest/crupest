eb11cc2ecae59971fe709fb8a7d0b4e2cdb24d93
crupest <crupest@outlook.com>
2023-10-03 20:56:33 +0800

    Update nvim configs. Detect brew clangd.

--- configs/nvim/lua/crupest/nvim/lsp/c.lua
+++ configs/nvim/lua/crupest/nvim/lsp/c.lua
@@ -1,9 +1,19 @@
 local lspconfig = require("lspconfig")
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
+local get_exe = require("crupest.system.find").get_exe
+
+local brew_clangd_path = "/usr/local/opt/llvm/bin/clangd"
 
 local function setup_lsp_c()
+    local clangd = "clangd"
+
+    if get_exe(brew_clangd_path) then
+        clangd = brew_clangd_path
+    end
+
     -- setup lsp clangd
     lspconfig.clangd.setup {
+        cmd = { clangd },
         capabilities = capabilities,
         on_attach = function(_, bufnr)
             vim.keymap.set('n', '<space>s', "<cmd>ClangdSwitchSourceHeader<cr>", {
