0c4994706ebc127ab48472c3384ec964c914ee1d
crupest <crupest@outlook.com>
2023-10-03 16:09:49 +0800

    Update nvim config. Improve formatter.

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -89,7 +89,8 @@ require("toggleterm").setup {
 require("nvim-autopairs").setup {}
 
 -- setup formatter
-require("crupest.nvim.plugins.formatter").setup_formatter()
+local formatter = require("crupest.nvim.plugins.formatter")
+formatter.setup_formatter()
 
 -- setup lint
 local lint = require("crupest.nvim.plugins.lint")
@@ -156,9 +157,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
         vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
         vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
         vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-        vim.keymap.set('n', '<space>f', function()
-            vim.lsp.buf.format { async = true }
-        end, opts)
     end,
 })
 
@@ -183,6 +181,7 @@ vim.keymap.set('n', '<leader>l]', vim.diagnostic.goto_next)
 vim.keymap.set('n', '<leader>ll', vim.diagnostic.setloclist)
 vim.keymap.set('n', '<leader>lr', lint.run_lint)
 
+vim.keymap.set('n', '<space>f', formatter.run_formatter)
 
 vim.keymap.set("n", "<c-tab>", "<cmd>bnext<cr>")
 vim.keymap.set("n", "<c-s-tab>", "<cmd>bNext<cr>")
