b3a15352c7019c9b6b9a790aba55e71454e4a2fa
crupest <crupest@outlook.com>
2023-11-13 13:46:33 +0800

    (docker:debian-dev) Add more dev aliases.

--- docker/debian-dev/bootstrap/bash-profile/dev.bash
+++ docker/debian-dev/bootstrap/bash-profile/dev.bash
@@ -1,6 +1,6 @@
 alias apt-get-build-dep-arm64="apt-get build-dep -aarm64 --arch-only"
+alias apt-get-build-dep-indep="apt-get build-dep --indep-only"
 alias dpkg-buildpackage-arm64="CONFIG_SITE=/etc/dpkg-cross/cross-config.arm64  DEB_BUILD_OPTIONS=nocheck dpkg-buildpackage -aarm64 -Pcross,nocheck"
 alias dpkg-buildpackage-arm64-build="dpkg-buildpackage-arm64 -T binary-arch"
-alias dpkg-buildpackage-arm64-clean="dpkg-buildpackage-arm64 -T clean"
-alias dpkg-buildpackage-indep="dpkg-buildpackage -T binary-indep"
+alias dpkg-buildpackage-indep-build="dpkg-buildpackage -T binary-indep"
 alias dpkg-buildpackage-clean="dpkg-buildpackage -T clean"
