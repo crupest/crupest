2a81b3061fdbc62dcab482cba0fd8f870652e55c
crupest <crupest@outlook.com>
2023-11-13 13:47:29 +0800

    (docker:debian-dev) add debhelper.

--- docker/debian-dev/bootstrap/setup-dev-tools.bash
+++ docker/debian-dev/bootstrap/setup-dev-tools.bash
@@ -2,5 +2,5 @@
 
 set -e
 
-apt-get install -y build-essential cpio kmod git bc python bison flex rsync libelf-dev libssl-dev libncurses-dev dwarves
+apt-get install -y build-essential debhelper cpio kmod git bc python bison flex rsync libelf-dev libssl-dev libncurses-dev dwarves
 
