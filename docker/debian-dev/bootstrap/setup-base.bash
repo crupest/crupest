b707256963c75c8e61509f5b339ad4a64edaa08a
crupest <crupest@outlook.com>
2023-11-21 18:48:08 +0800

    docker(debian-dev): try to fix sbuild.

--- docker/debian-dev/bootstrap/setup-base.bash
+++ docker/debian-dev/bootstrap/setup-base.bash
@@ -2,7 +2,7 @@
 
 set -e
 
+apt-get update
 apt-get install -y vim less man curl bash-completion rsync
 
 cat /bootstrap/bash-profile/bash-completion.bash >> /root/.bash_profile
-
--- docker/debian-dev/bootstrap/setup-dev-tools-arm64.bash
+++ docker/debian-dev/bootstrap/setup-dev-tools-arm64.bash
@@ -6,4 +6,3 @@ dpkg --add-architecture arm64
 apt-get update
 
 apt-get install -y crossbuild-essential-arm64
-
