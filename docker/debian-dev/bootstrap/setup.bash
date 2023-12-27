0e8f8eebe1543dafb10ed0d1f7087ffa89a295ea
crupest <crupest@outlook.com>
2023-12-27 23:58:24 +0800

    docker(debian-dev): add support to disable sbuild.

--- docker/debian-dev/bootstrap/setup.bash
+++ docker/debian-dev/bootstrap/setup.bash
@@ -7,4 +7,13 @@ export DEBIAN_FRONTEND=noninteractive
 /bootstrap/apt-source/setup.bash
 /bootstrap/setup-base.bash
 /bootstrap/setup-dev.bash
-/bootstrap/sbuild/setup.bash
+
+. /bootstrap/func.bash
+
+if is_true "$SETUP_SBUILD"; then
+    echo "Setup sbuild..."
+    /bootstrap/sbuild/setup.bash
+else
+    echo "Sbuild is disabled. Skipped."
+fi
+
