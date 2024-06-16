30ec0d5fdb5c13cad8f696a52ba8bdc51a73a138
crupest <crupest@outlook.com>
2024-06-16 11:38:17 +0800

    fix(docker/debian-dev): code-server log dir creation.

--- docker/crupest-debian-dev/bootstrap/start/code-server.bash
+++ docker/crupest-debian-dev/bootstrap/start/code-server.bash
@@ -8,7 +8,6 @@ CODE_SERVER_PORT=8080
 if which "$CODE_SERVER_PROGRAM" > /dev/null 2>&1; then
     if ! pgrep -f "$CODE_SERVER_PROGRAM" > /dev/null 2>&1; then
         echo "code-server is not running, starting..."
-        mkdir -p ~/.local/share/code-server
         "$CODE_SERVER_PROGRAM" "--bind-addr" "0.0.0.0:$CODE_SERVER_PORT"
     else
         echo "code-server is already running."
