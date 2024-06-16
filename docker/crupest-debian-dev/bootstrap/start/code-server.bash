c5177018c6fa2367ca4798a71f946e954654c6d2
crupest <crupest@outlook.com>
2024-06-16 11:38:17 +0800

    fix(docker/debian-dev): the way detect whehter code-server is running.

--- docker/crupest-debian-dev/bootstrap/start/code-server.bash
+++ docker/crupest-debian-dev/bootstrap/start/code-server.bash
@@ -6,7 +6,7 @@ CODE_SERVER_PROGRAM=code-server
 CODE_SERVER_PORT=8080
 
 if which "$CODE_SERVER_PROGRAM" > /dev/null 2>&1; then
-    if ! pgrep -f "$CODE_SERVER_PROGRAM" > /dev/null 2>&1; then
+    if ! pgrep -x "$CODE_SERVER_PROGRAM" > /dev/null 2>&1; then
         echo "code-server is not running, starting..."
         "$CODE_SERVER_PROGRAM" "--bind-addr" "0.0.0.0:$CODE_SERVER_PORT"
     else
