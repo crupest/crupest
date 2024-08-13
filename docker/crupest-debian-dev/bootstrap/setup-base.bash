18e9bb5c63702be90076b6225eb4a9781f4fb088
crupest <crupest@outlook.com>
2024-08-14 00:28:12 +0800

    fix(docker/debian-dev): python and setup-code-server.

--- docker/crupest-debian-dev/bootstrap/setup-base.bash
+++ docker/crupest-debian-dev/bootstrap/setup-base.bash
@@ -4,7 +4,7 @@ set -e
 
 . /bootstrap/func.bash
 
-apt-get install -y locales vim less man bash-completion rsync curl wget
+apt-get install -y locales procps vim less man bash-completion rsync curl wget
 
 localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8
 
