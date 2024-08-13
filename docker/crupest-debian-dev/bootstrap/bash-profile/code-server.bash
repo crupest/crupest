b92f69c4b985f3ac3d33d21d76a6b7c951cb3446
crupest <crupest@outlook.com>
2024-08-14 00:28:12 +0800

    feat(docker/debian-dev): add more output, rename vars and fix some bugs.

--- docker/crupest-debian-dev/bootstrap/bash-profile/code-server.bash
+++ docker/crupest-debian-dev/bootstrap/bash-profile/code-server.bash
@@ -6,7 +6,7 @@ CODE_SERVER_PORT=8080
 if which "$CODE_SERVER_PROGRAM" > /dev/null 2>&1; then
     if ! pgrep -f "$CODE_SERVER_PROGRAM" > /dev/null 2>&1; then
         mkdir -p ~/.local/share/code-server
-        nohup "$CODE_SERVER_PROGRAM" "--bind-addr", "0.0.0.0:$CODE_SERVER_PORT" \
+        "$CODE_SERVER_PROGRAM" "--bind-addr", "0.0.0.0:$CODE_SERVER_PORT" \
             > ~/.local/share/code-server/log  2> ~/.local/share/code-server/error &
     fi
 fi
--- docker/crupest-debian-dev/bootstrap/setup-base.bash
+++ docker/crupest-debian-dev/bootstrap/setup-base.bash
@@ -4,8 +4,17 @@ set -e
 
 . /bootstrap/func.bash
 
-apt-get install -y locales procps vim less man bash-completion rsync curl wget
+echo "Setting up basic system function..."
 
+echo "Installing basic packages..."
+apt-get install -y apt-utils
+apt-get install -y locales procps vim less man bash-completion rsync curl wget
+echo "Installing basic packages done."
+
+echo "Setting up locale..."
 localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8
+echo "Setting up locale done."
 
 append-bash-profile bash-completion.bash
+
+echo "Setting up basic system function done."
