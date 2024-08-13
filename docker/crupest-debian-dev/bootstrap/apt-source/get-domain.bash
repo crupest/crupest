3b1e2001e28b5b6295fc51ef5490d459b694df69
crupest <crupest@outlook.com>
2024-08-14 00:28:12 +0800

    fix(docker/debian-dev): mirror replace scripts on debian 12.

--- docker/crupest-debian-dev/bootstrap/apt-source/get-domain.bash
+++ docker/crupest-debian-dev/bootstrap/apt-source/get-domain.bash
@@ -2,4 +2,5 @@
 
 set -e
 
-sed "s|.*https\?://\([-_.a-zA-Z0-9]\+\)/.*|\\1|;q" /etc/apt/sources.list
+grep -e 'URIs:' /etc/apt/sources.list.d/debian.sources | \
+    sed -E 's|URIs:\s*https?://([-_.a-zA-Z0-9]+)/.*|\1|;q'
