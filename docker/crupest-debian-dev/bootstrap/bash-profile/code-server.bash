8cb9b351b122ef17b6026cce0d45432c5c28745d
crupest <crupest@outlook.com>
2024-08-14 00:28:12 +0800

    fix(docker/debian-dev): code-server args and work dir.

--- docker/crupest-debian-dev/bootstrap/bash-profile/code-server.bash
+++ docker/crupest-debian-dev/bootstrap/bash-profile/code-server.bash
@@ -7,7 +7,7 @@ if which "$CODE_SERVER_PROGRAM" > /dev/null 2>&1; then
     if ! pgrep -f "$CODE_SERVER_PROGRAM" > /dev/null 2>&1; then
         echo "code-server is not running, starting..."
         mkdir -p ~/.local/share/code-server
-        "$CODE_SERVER_PROGRAM" "--bind-addr", "0.0.0.0:$CODE_SERVER_PORT" \
+        "$CODE_SERVER_PROGRAM" "--bind-addr" "0.0.0.0:$CODE_SERVER_PORT" \
             > ~/.local/share/code-server/log  2> ~/.local/share/code-server/error &
     fi
 else
