453a1efefcd5088b34331cad3df91a0b906d0254
crupest <crupest@outlook.com>
2024-11-11 01:12:29 +0800

    HALF WORK: 2024.11.12

--- tools/cru-py/cru/util/__init__.py
+++ tools/cru-py/cru/util/__init__.py
@@ -1,20 +1,25 @@
-from ._const import make_unique_object, make_bool_unique_object, CRU_NOT_FOUND, CRU_USE_DEFAULT, CRU_DONT_CHANGE, \
+from typing import Any
+
+from ._const import cru_make_unique_object, cru_make_bool_unique_object, CRU_NOT_FOUND, CRU_USE_DEFAULT, \
+    CRU_DONT_CHANGE, \
     CRU_PLACEHOLDER
-from ._func import CruFunction, MetaFunction, RawFunctions, WrappedFunctions
+from ._func import CruFunction, CruFunctionMeta, CruRawFunctions, CruWrappedFunctions, CruFunctionGenerators
 from ._list import CruList, CruInplaceList, CruUniqueKeyInplaceList, ListOperations, CanBeList, ElementOperation, \
     ElementPredicate, ElementTransformer, OptionalElementOperation, ElementPredicate, OptionalElementTransformer
 from ._type import TypeSet
 
 F = CruFunction
-WF = WrappedFunctions
+WF = CruWrappedFunctions
+FG = CruFunctionGenerators
 L = CruList
+
 
 __all__ = [
     "CRU_NOT_FOUND", "CRU_USE_DEFAULT", "CRU_DONT_CHANGE", "CRU_PLACEHOLDER",
-    "CruFunction", "MetaFunction", "RawFunctions", "WrappedFunctions",
+    "CruFunction", "CruFunctionMeta", "CruRawFunctions", "CruWrappedFunctions", "CruFunctionGenerators",
     "CruList", "CruInplaceList", "CruUniqueKeyInplaceList", "ListOperations",
     "CanBeList", "ElementOperation", "ElementPredicate", "ElementTransformer",
     "OptionalElementOperation", "ElementPredicate", "OptionalElementTransformer",
     "TypeSet",
-    "F", "WF", "L"
+    "F", "WF", "FG", "L"
 ]
