b14e7bd7eddc73cf176eabefd9a293389d31a743
Yuqian Yang <crupest@crupest.life>
2025-02-13 11:14:10 +0800

    HALF WORK!

--- docker/debian-dev/bootstrap/apt-source/setup.bash
+++ docker/debian-dev/bootstrap/apt-source/setup.bash
@@ -1,28 +1,56 @@
 #! /usr/bin/env bash
+# shellcheck disable=1090,1091
 
 set -e
 
-dir=/bootstrap/apt-source
+die() {
+  echo "$@" >&2
+  exit 1
+}
 
-echo "Getting debian version..."
-debian_version=$("$dir/../get-debian-version.bash")
-
-if [[ -z $debian_version ]]; then
-    echo "Debian version not found."
-    exit 1
-else
-    echo "Debian version: $debian_version"
+if [[ $EUID -ne 0 ]]; then
+    die "This script must be run as root."
 fi
 
-if [[ $debian_version -ge 12 ]]; then
-    setup_dir=$dir/12
+os_release_file="/etc/os-release"
+if [[ -f "$os_release_file" ]]; then
+    debian_version=$(source "$os_release_file"; echo "$VERSION_CODENAME")
+    if [[ "$debian_version" != "bullseye|bookworm" ]]; then
+        die "This script can only be run on Debian Bullseye or Bookworm. But it is $debian_version"
+    fi
 else
-    setup_dir=$dir/11
+    die "$os_release_file not found. Failed to get debian version."
 fi
+
+
+apt_check_source() {
+    global apt_repo_file
+    old_one="/etc/apt/sources.list"
+    new_one="/etc/apt/sources.list.d/debian.sources"
+    if [[ -f "$old_one" ]]; then
+        echo "Found old format apt repo file at $old_one"
+        apt_repo_file="$old_one"
+    elif [[ -f "new_one" ]]; then
+        echo "Found new format apt 
+}
+
+apt_change_mirror() {
+    apt_check_source
+    echo "Change apt mirror to $1."
+    sed -i.bak "s|\(https\?://\)[-_.a-zA-Z0-9]\+/|\\1$1/|" /etc/apt/sources.list
+}
+
+apt_change_https() {
+    apt_check_source
+    echo "Change apt to use https."
+    sed -i 's|http://|https://|' /etc/apt/sources.list
+}
 
 echo "Setting up apt source..."
 
-if [[ -n $CRUPEST_DEBIAN_DEV_IN_CHINA ]]; then
+china_mirror="mirrors.ustc.edu.cn"
+
+if [[ -n $CRUPEST_IN_CHINA ]]; then
     echo "In China, using China source..."
     "$setup_dir/replace-domain.bash" "$(cat "$dir/china-source.txt")"
 fi
--- docker/debian-dev/bootstrap/setup.bash
+++ docker/debian-dev/bootstrap/setup.bash
@@ -2,11 +2,41 @@
 
 set -e
 
-export DEBIAN_FRONTEND=noninteractive
+is_true() {
+    if [[ "$1" =~ 1|on|true ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
 
-echo "Setting up crupest-debian-dev..."
+if [ ! -v CRUPEST_DEV ]; then
+    echo "CRUPEST_DEV is not set, defaulting to 1."
+    CRUPEST_DEV=1
+elif is_true $CRUPEST_DEV; then
+    echo "CRUPEST_DEV is set to true."
+else
+    echo "CRUPEST_DEV is set to false."
+fi
 
-. /bootstrap/func.bash
+echo "Begin to setup debian..."
+
+if [ -t 1 ]; then
+    echo "Looks like we are in a non-interactive environment. Setting DEBIAN_FRONTEND to noninteractive."
+    export DEBIAN_FRONTEND=noninteractive
+fi
+
+append-bash-profile() {
+    cat "/bootstrap/bash/$1" >> /home/$CRUPEST_DEBIAN_DEV_USER/.bash_profile
+}
+
+append-bashrc() {
+    cat "/bootstrap/bash/$1" >> /home/$CRUPEST_DEBIAN_DEV_USER/.bashrc
+}
+
+copy-home-dot-file() {
+    cp "/bootstrap/home-dot/$1" "/home/$CRUPEST_DEBIAN_DEV_USER/.$1"
+}
 
 /bootstrap/apt-source/setup.bash
 
@@ -17,6 +47,11 @@ echo "Updating apt source index done."
 /bootstrap/setup-user.bash
 /bootstrap/setup-base.bash
 /bootstrap/setup-dev.bash
+
+if is_true "$CRUPEST_FOR_DEV"; then
+    echo "CRUPEST_FOR_DEV is true, setting up dev environment..."
+    /bootstrap/setup-dev.bash
+fi
 
 if is_true "$CRUPEST_DEBIAN_DEV_SETUP_CODE_SERVER"; then
     echo "CRUPEST_DEBIAN_DEV_SETUP_CODE_SERVER is true, setting up code-server..."
