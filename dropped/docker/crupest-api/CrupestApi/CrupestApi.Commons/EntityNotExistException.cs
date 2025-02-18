b524986daf378cfe7aeafddafa98e9f978d26729
Yuqian Yang <crupest@crupest.life>
2025-02-19 02:05:39 +0800

    chore(docker): remove crupest-api, forgejo and move dropped.

--- dropped/docker/crupest-api/CrupestApi/CrupestApi.Commons/EntityNotExistException.cs
+++
@@ -1,8 +0,0 @@
-namespace CrupestApi.Commons;
-
-public class EntityNotExistException : Exception
-{
-    public EntityNotExistException() { }
-    public EntityNotExistException(string message) : base(message) { }
-    public EntityNotExistException(string message, Exception inner) : base(message, inner) { }
-}
