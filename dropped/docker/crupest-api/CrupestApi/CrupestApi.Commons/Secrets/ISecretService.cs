4c5df72057fe02257e243de37930a47425a84722
Yuqian Yang <crupest@crupest.life>
2025-02-19 02:05:39 +0800

    chore(docker): remove crupest-api, forgejo and move dropped.

--- dropped/docker/crupest-api/CrupestApi/CrupestApi.Commons/Secrets/ISecretService.cs
+++
@@ -1,8 +0,0 @@
-namespace CrupestApi.Commons.Secrets;
-
-public interface ISecretService
-{
-    void CreateTestSecret(string key, string secret);
-
-    List<string> GetPermissions(string secret);
-}
