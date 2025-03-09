ac7c3e0ab3b460a5a96df04496e6f310ef9572d0
Yuqian Yang <crupest@crupest.life>
2025-03-09 17:31:21 +0800

    feat(service): mv config to docker imagei, better pri/pub git.

--- services/docker/nginx/nginx-wrapper.bash
+++ services/docker/nginx/nginx-wrapper.bash
@@ -1,7 +1,16 @@
 #!/usr/bin/bash
 
-set -e
+set -e -o pipefail
+
+die() {
+  echo "$@" >&2
+  exit 1
+}
+
+[[ -n "$CRUPEST_DOMAIN" ]] || die "CRUPEST_DOMAIN is not set. It is used as root domain."
+[[ -n "$CRUPEST_GITHUB" ]] || die "CRUPEST_GITHUB is not set. It is used as GitHub redirection."
+[[ -n "$CRUPEST_V2RAY_PATH" ]] || die "CRUPEST_V2RAY_PATH is not set. It is used as v2ray tunnel endpoint."
 
 /app/certbot.bash &
 
-nginx "-g" "daemon off;"
+/docker-entrypoint.sh nginx "-g" "daemon off;"
