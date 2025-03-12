473e71165e47f029d1a578856ab936dd6f4bf9ed
Yuqian Yang <crupest@crupest.life>
2025-03-12 22:50:12 +0800

    feat(nvim): improve configs a lot.

--- store/home/config/nvim/lua/crupest/nvim/keymap.lua
+++
@@ -1,9 +0,0 @@
-local function setup()
-    vim.keymap.set("n", "<c-tab>", "<cmd>bnext<cr>")
-    vim.keymap.set("n", "<c-s-tab>", "<cmd>bNext<cr>")
-    vim.keymap.set("n", "<esc>", require("crupest.utils.nvim").close_float)
-end
-
-return {
-    setup = setup
-}
