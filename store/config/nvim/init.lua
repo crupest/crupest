ee722b324a7918d79da02e9f54f57d3a416a396a
Yuqian Yang <crupest@crupest.life>
2025-06-17 21:35:13 +0800

    nvim: remove unneeded bins and disable scroll animation of neovide.

--- store/config/nvim/init.lua
+++ store/config/nvim/init.lua
@@ -16,11 +16,18 @@ require("lazy").setup {
     spec = { { import = "plugins" } }
 }
 
+vim.cmd([[
+    if has('nvim') && executable('nvr')
+        let $GIT_EDITOR = 'nvr -cc split --remote-wait'
+    endif
+]])
+
 if vim.g.neovide then
     vim.opt.guifont = "Maple Mono";
     vim.g.neovide_normal_opacity = 0.95;
     vim.g.neovide_input_ime = false;
     vim.g.neovide_cursor_animate_in_insert_mode = false
+    vim.g.neovide_scroll_animation_far_lines = 0
     vim.g.neovide_input_macos_option_key_is_meta = 'only_left'
     vim.cmd("colorscheme catppuccin-macchiato")
 end
