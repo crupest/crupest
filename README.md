5f00fdd06fa51bb0c82928e060d3f8811bc171e9
Yuqian Yang <crupest@crupest.life>
2025-06-30 14:25:02 +0800

    mail: revert removing.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Hi! This is **crupest**. Nice to meet you here! 🤗
-Feel free to contact me via my email address *<crupest@outlook.com>*,
+Feel free to contact me via my email address *<crupest@crupest.life>*,
 or just create an issue in any of my [GitHub](https://github.com/crupest)
 repos. I love talking with people a lot.
--- services/docker/nginx/nginx-wrapper.bash
+++ services/docker/nginx/nginx-wrapper.bash
@@ -7,10 +7,6 @@ die() {
   exit 1
 }
 
-[[ -n "$CRUPEST_DOMAIN" ]] || die "CRUPEST_DOMAIN is not set. It is used as root domain."
-[[ -n "$CRUPEST_GITHUB" ]] || die "CRUPEST_GITHUB is not set. It is used as GitHub redirection."
-[[ -n "$CRUPEST_V2RAY_PATH" ]] || die "CRUPEST_V2RAY_PATH is not set. It is used as v2ray tunnel endpoint."
-
 /app/certbot.bash &
 
 /docker-entrypoint.sh nginx "-g" "daemon off;"
