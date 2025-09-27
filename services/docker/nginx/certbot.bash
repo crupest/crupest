4c7253a9131f8a5f966c9f1dd4a07131c7b9fe86
Yuqian Yang <crupest@crupest.life>
2025-09-27 21:32:25 +0800

    fix: certbot use webroot to renew.

--- services/docker/nginx/certbot.bash
+++ services/docker/nginx/certbot.bash
@@ -2,8 +2,11 @@
 
 set -e
 
+echo "Sleep 5 seconds waiting for nginx to start."
+sleep 5s
+
 while true; do
-    certbot renew --deploy-hook "nginx -s reload"
+    certbot renew --webroot -w /var/www/certbot --deploy-hook "nginx -s reload"
     echo "Sleep one day before next certbot renew."
     sleep 1d
 done
