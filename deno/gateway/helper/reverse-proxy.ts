f0fe8808a079e698aa27dff57a28f141ed8326cc
Yuqian Yang <crupest@crupest.life>
2026-03-27 20:04:35 +0800

    feat: gateway websocket support.

--- deno/gateway/helper/reverse-proxy.ts
+++ deno/gateway/helper/reverse-proxy.ts
@@ -1,6 +1,40 @@
 import { Context } from "hono";
 import { getConnInfo } from "hono/deno";
 
+function isWebSocketRequest(headers: Headers) {
+  return headers.get("upgrade")?.toLowerCase() === "websocket";
+}
+
+type WebSocketData = string | Blob | ArrayBufferLike | ArrayBufferView;
+
+function forwardWebSocket(from: WebSocket, to: WebSocket) {
+  const queue: WebSocketData[] = [];
+  from.addEventListener("message", (event) => {
+    switch (to.readyState) {
+      case WebSocket.OPEN:
+        to.send(event.data);
+        break;
+      case WebSocket.CONNECTING:
+        queue.push(event.data);
+        break;
+    }
+  });
+  to.addEventListener("open", () => {
+    queue.forEach((data) => to.send(data));
+    queue.length = 0;
+  });
+  from.addEventListener("close", (event) => {
+    if (to.readyState === WebSocket.OPEN) {
+      to.close(event.code, event.reason);
+    }
+  });
+  from.addEventListener("error", () => {
+    if (to.readyState === WebSocket.OPEN) {
+      to.close(1011, "WebSocket error");
+    }
+  });
+}
+
 export function createReverseProxyHandler(
   { originServer }: { originServer: string },
 ) {
@@ -8,12 +42,12 @@ export function createReverseProxyHandler(
     const url = new URL(c.req.url);
     const { host, protocol } = url;
     const remoteAddr = getConnInfo(c).remote.address;
+    const headers = new Headers(c.req.header());
+    const isWebSocket = isWebSocketRequest(headers);
 
-    url.protocol = "http:";
+    url.protocol = isWebSocket ? "ws:" : "http:";
     url.host = originServer;
 
-    const headers = new Headers(c.req.header());
-    headers.set("Connection", headers.get("upgrade") ? "upgrade" : "close");
     headers.set("Host", host);
     headers.set("X-Forwarded-Host", host);
     headers.set("X-Forwarded-Proto", protocol.slice(0, -1));
@@ -24,6 +58,23 @@ export function createReverseProxyHandler(
         "X-Forwarded-For",
         forwardedFor ? `${forwardedFor}, ${remoteAddr}` : remoteAddr,
       );
+    }
+
+    if (isWebSocket) {
+      const { socket: downstreamSocket, response } = Deno.upgradeWebSocket(
+        c.req.raw,
+      );
+      const upstreamSocket = new WebSocket(
+        url.toString(),
+        {
+          headers,
+        },
+      );
+
+      forwardWebSocket(downstreamSocket, upstreamSocket);
+      forwardWebSocket(upstreamSocket, downstreamSocket);
+
+      return response;
     }
 
     return await fetch(url, {
