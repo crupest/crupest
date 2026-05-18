f49a4fb0b78ef579ce03f4bd675d00b5cf82021f
Yuqian Yang <crupest@crupest.life>
2026-05-18 19:59:41 +0800

    feat: migrate nvim treesitter config.

--- store/config/nvim/lua/setup/plugins/tree-sitter.lua
+++ store/config/nvim/lua/setup/plugins/tree-sitter.lua
@@ -3,12 +3,18 @@ local has_setup = false
 local function setup()
     if has_setup then return end
 
-    require 'nvim-treesitter.configs'.setup {
-        highlight = { enable = true },
-        incremental_selection = { enable = true },
-        textobjects = { enable = true },
-    }
+    require 'nvim-treesitter'.setup {}
 
+    vim.api.nvim_create_autocmd('FileType', {
+        callback = function()
+            local ok = pcall(vim.treesitter.start)
+            if ok then
+                vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+                vim.wo[0][0].foldmethod = 'expr'
+                vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            end
+        end,
+    })
     has_setup = true
 end
 
